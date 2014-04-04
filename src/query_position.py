e9d0a0ba9d2c1bdaa3b1d095c30af4caa546d181
Carlos Brandt <carloshenriquebrandt@gmail.com>
2014-04-04 12:31:23 +0200

    Completed the CLI implementation. The user gives catalog, ra, dec and a radius.

--- src/query_position.py
+++ src/query_position.py
@@ -1,42 +1,57 @@
+import warnings
+warnings.simplefilter("ignore")
+
 from astropy.vo.client import conesearch as cone
 from astropy.coordinates import ICRS
 from astropy import units as u
 
-import warnings
+#cone.list_catalogs()
+descr = """
+    This script searchs for sources in a given position (RA,DEC) and radius (R) 
+    in a given catalog. The catalogs available to search are:
+    'sdss'
+    '2mass'
+    'usno'
+"""
+catalogs = {
+    'sdss' : 'SDSS DR7 - Sloan Digital Sky Survey Data Release 7 3',
+    '2mass': 'Two Micron All Sky Survey (2MASS) 2',
+    'usno' : 'USNO-A2.0 1'
+}
 
-warnings.simplefilter("ignore")
-
-def query_position(ra,dec,rad,ru):
+def query_position(ra,dec,rad,ru,cat):
 
     radius = rad*ru
-
-    #cone.list_catalogs()
-    # Catalogs
-    cats = {'sdss7' : 'SDSS DR7 - Sloan Digital Sky Survey Data Release 7 3'}
-    cats.update({'2mass' : 'Two Micron All Sky Survey (2MASS) 2'})
     
     c = ICRS(ra,dec,unit=(u.degree,u.degree))
     
     print "---"
-    print "\033[37m-> source at (ra,dec): (%s,%s)\033[0m" % (c.ra,c.dec)
+    print "\033[37m-> Looking for source(s) [%s] around (ra=%s,dec=%s)\033[0m" % (radius,c.ra,c.dec)
     print "---"
-    for cat in cats.values():
 
-        try:
-            match = cone.conesearch(c, radius, catalog_db=cat)
-        except:
-            print "---"
-            print "\033[91mNot able to access data for source in archive %s\033[0m" % (cat)
-            print "---"
-            continue
-        
+    try:
+        match = cone.conesearch(c, radius, catalog_db=cat)
+    except:
         print "---"
-        print "Found data for source in \033[92m%s\033[0m" % (cat)
-        print " -> search radio: %s" % radius
-        print " -> position of the source (ra,dec): \033[93m(%s,%s)\033[0m" % (c.ra,c.dec)
-        print " Data [ra / dec]:"
-        print zip(match.array['ra'].data, match.array['dec'])
-        print "---"    
+        print "\033[91mNot able to access data for source in archive %s\033[0m" % (cat)
+        print "---"
+        return None
+    
+    print "---"
+    print "Found data for source in \033[92m%s\033[0m" % (cat)
+    print " -> search radio: %s" % radius
+    print " -> position of the source (ra,dec): \033[93m(%s,%s)\033[0m" % (c.ra,c.dec)
+    print " Data [ra / dec]:"
+    print zip(match.array['ra'].data, match.array['dec'])
+    print "---"    
+
+    return match
+    
+def list_catalogs():
+    print "Available options for 'catalogs':"
+    for k,v in catalogs.items():
+        print "-> %s : %s" % (k,v)
+    
 
 #res.array.size
 #res.array.dtype
@@ -48,17 +63,20 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('ra', help="Right Ascension of the object (in degrees by default)")
-    parser.add_argument('dec', help="Declination of the object (in degrees by default)")
-    parser.add_argument('radius', type=float,
+    parser.add_argument('--ra', dest='ra', required=True,
+                        help="Right Ascension of the object (in degrees by default)")
+    parser.add_argument('--dec', dest='dec', required=True,
+                        help="Declination of the object (in degrees by default)")
+    parser.add_argument('-r','--radius', type=float, dest='radius', required=True,
                         help="Radius (around RA,DEC) to search for object(s)",)
 
-#    parser.add_argument('--cUnit', choice=('degree','arcmin','arcsec'), default='degree',
-#                        help="Unit used on coordinates, RA *and* DEC.'")
     parser.add_argument('--rUnit', default='arcsec',
                         help="Unit used for radius value. Choices are 'arcmin','arcsec'. Default is 'arcsec'.")
+
+    parser.add_argument('-c','--catalog', dest='cat', required=True,
+                        help="Catalog to search for data. To see your choices use the '--list' option.")
+                        
     args = parser.parse_args()
-    
     ra = args.ra
     dec = args.dec
     radius = args.radius
@@ -68,8 +86,11 @@ if __name__ == '__main__':
     elif args.rUnit == 'arcsec':
         ru = u.arcsec
     else:
-        raise ValueError("Radius' unit not valid. Try one of the choices: degree, arcmin, arcsec.")
+        raise ValueError("Radius' unit not valid. Use 'arcmin' or 'arcsec'.")
+
+    cat = args.cat
+    if cat not in catalogs.keys():
+        raise ValueError("Catalog is not known. Try a valid one (-h).")
     
-    print ru
-    out = query_position(ra,dec,radius,ru)
+    out = query_position(ra,dec,radius,ru,cat)
     
